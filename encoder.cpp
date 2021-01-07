358abf16488d36406a0af75b991e2724e2238afb
Tim Minear <tminear13@gmail.com>
2021-01-06 19:33:16 -0500

    encode added as function, refactored fetch_image to convert the string buffer to a vector of unsigned chars. Might be a way to avoid all of this in the callback function, but this way works.

--- encoder.cpp
+++ encoder.cpp
@@ -2,6 +2,11 @@
 #include "curl/curl.h"
 #include <cstdlib>
 #include <iostream>
+#include <vector>
+#include <fstream>
+
+
+static const char base64_set[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 /**
  * callback function for use by CURLOTP_WRITEFUNCTION -> overrides fwrite as default callback. Allows us to manipulate
@@ -28,7 +33,7 @@ static size_t callback_buffer(void *contents, size_t size, size_t nmemb, void *u
  * @param url - URL provided from argv
  * @return buffer - loaded buffer of data from the image, "" - empty string if cURL error happened
  */
-std::string fetch_image(char* url)
+std::vector<unsigned char> fetch_image(char* url)
 {
 
 	std::string buffer;
@@ -44,19 +49,62 @@ std::string fetch_image(char* url)
 
 	if (rc)//CURL_OK is 0, nonzero is an error
 	{
-		printf("Failed to download: %s\n cURL Error: %d\n", url, rc);
+		printf("Failed to download: %s\ncURL Error: %d\n", url, rc);
 
 		long res_code = 0; //check to see response code given
 
 		curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &res_code);
 		if (!(res_code == 200 || res_code == 201)) {
 			printf("Response code: %ld\n", res_code);
-			return "";
+			return std::vector<unsigned char>();
 		}
 	}
 
 	curl_easy_cleanup(curl);
 
-	return buffer;
+	std::vector<unsigned char> data(buffer.begin(), buffer.end());
+
+	return data;
+}
+
+/**
+ * Encodes the vector of unsigned characters into Base64 and writes the output to the argv file
+ * @param data - vector of unsigned characters obtained from fetching an image
+ * @param fileName - argv[2] the name of the desired output file
+ */
+void encode(std::vector<unsigned char> data, std::string fileName){
+
+	//dealing with 24-bits at a time so this covers if the vector is not a multiple of 3
+	if((data.size() % 3) != 0){
+		for(size_t i = 0; i < data.size() % 3; ++i){
+			data.push_back('\0');
+		}
+	}
+
+	//open file to write to
+	std::ofstream outFile;
+	outFile.open(fileName);
+
+	//24 bits is the smallest chunk to break the
+	for(size_t i = 0; i < data.size(); i+=3){
+
+		//Base64 inserts newlines every 76 characters,
+		if(i % 19 == 0 && i != 0){
+			outFile << '\n';
+		}
+
+		//grab three bytes at a time to break into 4 6-bit chunks
+		std::string bytes = std::bitset<8>(data[i]).to_string() + std::bitset<8>(data[i+1]).to_string()
+				+ std::bitset<8>(data[i+2]).to_string();
+
+		outFile << base64_set[std::stoi(bytes.substr(0,6), nullptr, 2)];
+		outFile << base64_set[std::stoi(bytes.substr(6,6), nullptr, 2)];
+		outFile << base64_set[std::stoi(bytes.substr(12,6), nullptr, 2)];
+		outFile << base64_set[std::stoi(bytes.substr(18,6), nullptr, 2)];
+
+	}
+
+	outFile.close();
+
 }
 
