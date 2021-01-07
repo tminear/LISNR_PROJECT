78d5f16531adc90fdac20ef680c1e5654be27641
Tim Minear <tminear13@gmail.com>
2021-01-06 19:30:30 -0500

    Added encode as a function, refactored fetch_image to return a vector of unsigned char.

--- encoder.h
+++ encoder.h
@@ -3,8 +3,10 @@
 
 #include <cstdlib>
 #include <string>
+#include <vector>
 
 static size_t callback_buffer(void *contents, size_t size, size_t nmemb, void *user_data);
-std::string fetch_image(char* url);
+std::vector<unsigned char> fetch_image(char* url);
+void encode(std::vector<unsigned char> data, std::string fileName);
 
 #endif //LISNR_PROJECT_ENCODER_H
